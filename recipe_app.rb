3bca402b0f18c50703f014e000e7e825f53aca9b
sofiajeanne <sofia.rogg@gmail.com>
2016-10-17 12:16:53 -0400

    created parallel views for retrieval optimization

--- recipe_app.rb
+++ recipe_app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recipe_app.db")
- 
+
 class Recipe
   include DataMapper::Resource
   property :id, Serial
@@ -10,7 +10,7 @@ class Recipe
   property :instructions, Text
   property :created_at, DateTime
   
-  has n, :ingredients
+  has n, :relations
 end
  
 class Ingredient
@@ -18,13 +18,34 @@ class Ingredient
   property :id, Serial
   property :name, String
 
-  belongs_to :recipe
+  has n, :relations
 end
+
+class Relation
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :amount, String
+  property :unit, String
+
+  belongs_to :recipe
+  belongs_to :ingredient
+end
+
 
 DataMapper.finalize.auto_upgrade!
 
+
 get '/' do
-  @recipes = Recipe.all :order => :id.desc
-  @ingredients = Ingredient.all :order => :id.desc
+  @recipes = Recipe.all :order => :id.asc
+  @ingredients = Ingredient.all :order => :id.asc
+  @relations = Relation.all :order => :id.asc
   erb :home
+end
+
+get '/join' do
+  @recipes = Recipe.all :order => :id.asc
+  @ingredients = Ingredient.all :order => :id.asc
+  @relations = Relation.all :order => :id.asc
+  erb :join
 end
